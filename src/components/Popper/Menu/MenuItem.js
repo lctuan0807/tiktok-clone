d1ad3c759acf3eb4c445370cee4f9a2a929aba08
lctuan0807 <tuan.le@readytech.io>
2022-06-20 10:23:56 +0700

    header #5

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,13 +1,12 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import Button from '~/components/Button';
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ item }) {
+function MenuItem({ item, onClick }) {
   return (
-    <Button className={cx('menu-item')} leftIcon={item.icon} to={item.to}>
+    <Button className={cx('menu-item')} leftIcon={item.icon} to={item.to} onClick={onClick}>
       {item.title}
     </Button>
   );
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -3,13 +3,35 @@ import Tippy from '@tippyjs/react/headless';
 import styles from './Menu.module.scss';
 import { Wrapper as PopperWraper } from '~/components/Popper';
 import MenuItem from './MenuItem';
-import { Children } from 'react';
+import { useState } from 'react';
+import Header from './Header';
 
 const cx = classNames.bind(styles);
 
-function Menu({ items, children }) {
+const defaultFn = () => {};
+
+function Menu({ items = [], children, onChange = defaultFn }) {
+  const [history, setHistory] = useState([{ data: items }]);
+  const current = history[history.length - 1];
+
   const renderItems = () => {
-    return items.map((item, index) => <MenuItem key={index} item={item} />);
+    return current.data.map((item, index) => {
+      const isParent = !!item.children;
+
+      return (
+        <MenuItem
+          key={index}
+          item={item}
+          onClick={() => {
+            if (isParent) {
+              setHistory((prev) => [...prev, item.children]);
+            } else {
+              onChange(item);
+            }
+          }}
+        />
+      );
+    });
   };
 
   return (
@@ -19,7 +41,17 @@ function Menu({ items, children }) {
       placement="bottom-end"
       render={(attrs) => (
         <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-          <PopperWraper className={cx('menu-popper')}>{renderItems()}</PopperWraper>
+          <PopperWraper className={cx('menu-popper')}>
+            {history.length > 1 && (
+              <Header
+                title={current.title || 'Language'}
+                onBack={() => {
+                  setHistory((prev) => prev.slice(0, history.length - 1));
+                }}
+              />
+            )}
+            {renderItems()}
+          </PopperWraper>
         </div>
       )}
     >
